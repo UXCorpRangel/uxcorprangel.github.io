ff82f7749cfd430fa77a8947bd50d40e8f6098ca
Jaime Alan Gutiérrez Cruz <33694882+jagcruz@users.noreply.github.com>
2024-10-08 00:41:24 -0300

    chore: :tada: start project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://uxcorprangel.github.io',
+  trailingSlash: 'never',
+  integrations: [icon()],
+  vite: {
+    css: {
+      transformer: 'lightningcss',
+      lightningcss: { drafts: { customMedia: true } }
+    },
+    build: { cssMinify: 'lightningcss' }
+  }
+});
