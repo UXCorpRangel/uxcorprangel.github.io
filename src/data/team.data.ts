a1db9e4f666ae7cfd87cc6751563c24c2e5a716e
Jaime Alan Gutiérrez Cruz <33694882+jagcruz@users.noreply.github.com>
2024-10-17 11:13:21 -0300

    refactor: update team member description

--- src/data/team.data.ts
+++ src/data/team.data.ts
@@ -99,7 +99,7 @@ export const team: TeamMember[] = [
       { label: 'Github', link: 'https://github.com/Facundodandrea', icon: 'github', color: 'var(--github-color)' },
       { label: 'LinkedIn', link: 'https://www.linkedin.com/in/facundo-d-andrea', icon: 'linkedin', color: 'var(--linkedin-color)' }
     ],
-    description: `Persona de baja estatura que programa, es conocido como tu amigo el apoya brazo. Es el terror de las bases de datos, capaz de centrar un <div> sin morir en el intento. Egresado como técnico superior en programación. Solo sabe hablar con referencias a memes. Aprendiz de Figma, que ya no explota el efecto "mirror" en tarjetas.`
+    description: `Persona de baja estatura que programa, es conocido como tu amigo el apoya brazo. Es el terror de las bases de datos, capaz de centrar un <div> sin morir en el intento. Egresado como técnico superior en programación. Solo sabe hablar con referencias a memes. Aprendiz de Figma que ya no explota el efecto "mirror" en tarjetas.`
   }
 ];
 
