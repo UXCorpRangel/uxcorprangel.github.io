b87fb0afa073d60b6cb2ef776274f7cac0d79350
Jaime Alan Gutiérrez Cruz <33694882+jagcruz@users.noreply.github.com>
2024-11-10 04:44:36 -0300

    feat: enhance Portfolio type with PortfolioLink for better link structure

--- src/contracts/portfolios.types.ts
+++ src/contracts/portfolios.types.ts
@@ -1,9 +1,14 @@
 import type { SocialIconLink } from './link.types';
 
+type PortfolioLink = SocialIconLink & {
+  /** Color del texto del enlace */
+  textColor: string;
+};
+
 export type Portfolio = {
   name: string;
   img: string;
   description: string;
   command: string;
-  links: (SocialIconLink & { textColor: string })[];
+  links: PortfolioLink[];
 };
